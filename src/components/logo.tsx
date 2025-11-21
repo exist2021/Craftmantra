758edb12d531613235263e48d32e5fd0e760530d
Manoj Nayak <manoj@manojnayak.com>
2025-11-21 10:12:23 +0000

    Use this logo https://i.postimg.cc/ZRfxfXJH/46e799a9-3699-405d-98bf-cb2b

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,11 +5,11 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center" aria-label="Craft Mantra Homepage">
       <Image
-        src="https://i.postimg.cc/fbKdHVcV/IMG-7530.png"
+        src="https://i.postimg.cc/ZRfxfXJH/46e799a9-3699-405d-98bf-cb2bd81daa5d.jpg"
         alt="Craft Mantra Logo"
-        width={200}
-        height={55}
-        className="h-14 w-auto"
+        width={56}
+        height={56}
+        className="h-14 w-14"
         priority
       />
     </Link>
